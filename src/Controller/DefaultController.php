04cdac93a6d6668ffaeeabcc1dfdb211b7a7f8c7
Andrew MacRobert <andrew.macrobert@gmail.com>
2021-05-22 09:00:04 -0400

    Fix Route depcrecation

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,12 +10,12 @@ use App\Service\PhotoCategory;
 use App\Service\WebContent;
 use Doctrine\ORM\EntityManagerInterface;
 use HieuLe\WordpressXmlrpcClient\WordpressClient;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
